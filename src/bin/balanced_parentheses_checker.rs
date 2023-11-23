81b3c80c7a3a5b0ed3090d355f9f0d54cc36aed4
Amos Elmaliah <amosel@gmail.com>
2023-11-22 22:12:47 -0500

    finished balanced parent checker problem.

--- src/bin/balanced_parentheses_checker.rs
+++ src/bin/balanced_parentheses_checker.rs
@@ -1,32 +1,46 @@
 fn main() {
-  let test_string = "[{()}]";
-  println!("Is '{}' balanced? {}", test_string, is_balanced(test_string));
+    let test_string = "[{()}]";
+    println!(
+        "Is '{}' balanced? {}",
+        test_string,
+        is_balanced(test_string)
+    );
 }
 
 fn is_balanced(s: &str) -> bool {
-  // TODO: Implement the logic to check if the string `s` is balanced.
-  unimplemented!()
+    let mut stack = Vec::new();
+    for char in s.chars() {
+      match char {
+          ')' => if stack.pop() != Some('(') { return false },
+          ']' => if stack.pop() != Some('[') { return false },
+          '}' => if stack.pop() != Some('{') { return false },
+          '(' | '[' | '{' => stack.push(char),
+          _ => {}
+      }
+    }
+    stack.is_empty()
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_balanced() {
-      assert!(is_balanced("()"));
-      assert!(is_balanced("[]"));
-      assert!(is_balanced("{}"));
-      assert!(is_balanced("({[]})"));
-      assert!(is_balanced(""));
-  }
+    #[test]
+    fn test_balanced() {
+        assert!(is_balanced("()"));
+        assert!(is_balanced("[]"));
+        assert!(is_balanced("{}"));
+        assert!(is_balanced("({[]})"));
+        assert!(is_balanced("(())"));
+        assert!(is_balanced("{([])}"));
+    }
 
-  #[test]
-  fn test_not_balanced() {
-      assert!(!is_balanced("(]"));
-      assert!(!is_balanced("([)]"));
-      assert!(!is_balanced("{[}]"));
-      assert!(!is_balanced("{"));
-      assert!(!is_balanced("}"));
-  }
+    #[test]
+    fn test_not_balanced() {
+        assert!(!is_balanced("(]"));
+        assert!(!is_balanced("([)]"));
+        assert!(!is_balanced("{[}]"));
+        assert!(!is_balanced("{"));
+        assert!(!is_balanced("}"));
+    }
 }
