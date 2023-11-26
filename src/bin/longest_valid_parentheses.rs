1b0a80c7beb7f71d9f7fb38606c2be4cafe7da1a
Amos Elmaliah <amosel@gmail.com>
2023-11-26 11:54:25 -0500

    longest valid parenthesis: simple makes perfect.

--- src/bin/longest_valid_parentheses.rs
+++ src/bin/longest_valid_parentheses.rs
@@ -36,48 +36,18 @@ fn main() {
 fn longest_valid_parentheses(str: &str) -> i32 {
     // the count of open parenthesis:
     let mut length = 0i32;
-    // A closing parenthesis without paired open paranthesis will invalidate a substring
-    // Support advancing the beginning of valid substring
-    let mut v: i32 = -1;
-    // Keep a tab of open paranthesis positions.
     let mut stack = Vec::new(); // stack of indices of open parenteses.
-
+    stack.push(-1);
     for (index, char) in str.chars().enumerate() {
-        match char {
-            '(' => {
-                stack.push(index as i32);
+        if char == '(' {
+            stack.push(index as i32);
+        } else {
+            stack.pop();
+            if stack.is_empty() {
+                stack.push(index as i32)
+            } else {
+                length = length.max(index as i32 - *stack.last().unwrap());
             }
-            ')' => {
-                // look for a matching opening parenthesis
-                if let Some(closing_parenthesis_index) = stack.pop() {
-                    // identify the valid substring:
-                    // 4 cases we cover here
-                    // 1. The open parenthesis pair is at the beginning of the valid substring, it is the signle memeber of the stack.
-                    // 2. The possibly valid substring start indicated with 'v' is before the index of the open parenthesis pair in which case there are two options:
-                    // 2.1 The substring starting at v and ending in the index of the closing parenthsis is not valid.
-                    // 2.2 The substring starting at v is valid.
-                    // 2.3 The substring starting at some index that is bigger than v is valid.
-                    // Here are examples of that:
-                    // Example of case 1: ')()' or '((((()))))' -- here the index of the opening parenthesis pair will be enough to get the length
-                    // Example of case 2.1: '(()' '(()(())(()' here the substring before the current closing parenthesis index is not valid.
-                    // Example of case 2.2: ')()()' '()(())' here substring starting at v is valid
-                    // Example of case 2.3: ')(()()' '(()(())' here only a slice of the substring starting at v is valid
-                    if closing_parenthesis_index == v {
-                        // deal with case 1:
-                        length = length.max((index as i32) - closing_parenthesis_index);
-                    } else if stack
-                        .last()
-                        .map_or(false, |&last| last == closing_parenthesis_index as i32 - 1)
-                    {
-                        length = length.max(2);
-                    } else {
-                        length = length.max((index as i32) - (v + (stack.len() as i32)));
-                    }
-                } else {
-                    v = index as i32;
-                }
-            }
-            _ => {}
         }
     }
     length
