312d944cc37fc18eb447a1015cbba090730c2f18
Amos Elmaliah <amosel@gmail.com>
2023-12-05 22:30:27 -0500

    https://leetcode.com/problems/kth-largest-element-in-an-array

---
+++ src/bin/kth_largest_element_in_an_array.rs
@@ -0,0 +1,26 @@
+pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+  let mut x = nums;
+  x.sort_unstable();
+  *x.get(x.len() - k as usize).unwrap()
+}
+
+fn main() {
+
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_findkth_largest() {
+    assert_eq!(
+      find_kth_largest(vec![3,2,1,5,6,4], 2),
+      5
+    );
+    assert_eq!(
+      find_kth_largest(vec![3,2,3,1,2,4,5,5,6], 4),
+      4
+    )
+  }
+}
